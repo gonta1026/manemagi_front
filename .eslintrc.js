a24593d07fc8fa525a5b3b810f9943470a2d09c4
gonta1026 <katano.original@gmail.com>
2021-06-22 01:10:29 +0900

    fix: eslintを修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,3 @@
-/*eslint-env node*/
 module.exports = {
   root: true,
   env: {
@@ -7,59 +6,15 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
-    'plugin:jsx-a11y/recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:prettier/recommended',
-    'prettier/@typescript-eslint',
-    'prettier/react',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
   ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 2020,
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint', 'react', 'import'],
+  plugins: ['react', '@typescript-eslint'],
   settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
-      },
-      typescript: {
-        config: 'tsconfig.json',
-        alwaysTryTypes: true,
-      },
+    react: {
+      version: 'detect',
     },
   },
-  rules: {
-    '@typescript-eslint/ban-types': [
-      'error',
-      {
-        types: {
-          '{}': false,
-        },
-      },
-    ],
-    'react/prop-types': ['off'],
-    'react/react-in-jsx-scope': 'off',
-    'react/jsx-filename-extension': ['error', { extensions: ['.jsx', '.tsx'] }],
-    'import/order': ['error'],
-    'prettier/prettier': [
-      'error',
-      {
-        trailingComma: 'all',
-        endOfLine: 'lf',
-        semi: false,
-        singleQuote: true,
-        printWidth: 80,
-        tabWidth: 2,
-      },
-    ],
-  },
+  rules: {},
 };
