65843ac8592df8929c167f9d84f7d68877e5d5df
gonta1026 <katano.original@gmail.com>
2021-06-25 00:19:08 +0900

    feat: pageMapを追加

---
+++ src/pageMap/index.ts
@@ -0,0 +1,28 @@
+// サイドバーなどのpageのリンクn仕様をする想定
+export const pageMap = [
+  {
+    name: '買い物一覧',
+    link: '/shopping',
+    icon: '',
+  },
+  {
+    name: '買い物登録',
+    link: '/shopping/new',
+    icon: '',
+  },
+  {
+    name: '請求一覧',
+    link: '/claim',
+    icon: '',
+  },
+  {
+    name: '請求登録',
+    link: '/claim/new',
+    icon: '',
+  },
+  {
+    name: '設定編集',
+    link: '/setting',
+    icon: '',
+  },
+];
