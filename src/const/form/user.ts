3708ae1cd3d11d143cfb9c16d25ab1049dd95ce1
gonta1026 <katano.original@gmail.com>
2021-06-29 23:42:17 +0900

    feat: USERFORM用の定数を追加

---
+++ src/const/form/user.ts
@@ -0,0 +1,18 @@
+export const USERFORM = {
+  NAME: {
+    LABEL: '名前',
+    ID: 'name',
+  },
+  EMAIL: {
+    LABEL: 'メールアドレス',
+    ID: 'email',
+  },
+  PASSWORD: {
+    LABEL: 'パスワード',
+    ID: 'password',
+  },
+  PASSWORD_CONFIRM: {
+    LABEL: 'パスワードの再確認',
+    ID: 'passwordConfirm',
+  },
+} as const;
