798b959816f9bafceeea9f15e97a94020c5e6a9a
gonta1026 <katano.original@gmail.com>
2021-07-03 15:26:28 +0900

    update: 中継ファイルを更新

--- src/components/common/uiParts/atoms/index.ts
+++ src/components/common/uiParts/atoms/index.ts
@@ -1,7 +1,9 @@
 export { default as BaseButton } from './button/BaseButton';
 export { default as BaseErrorMessagesWrapper } from './error/BaseErrorMessageWrapper';
 export { default as BaseLoading } from './loading/BaseLoading';
+export { default as BaseLink } from './link/BaseLink';
 export { default as BasePageTitle } from './title/BasePageTitle';
 export { default as BaseRequired } from './form/BaseRequired';
+export { default as BaseIcon } from './icon//BaseIcon';
 export { default as BaseLabel } from './form/BaseLabel';
 export { default as BaseTextField } from './form/BaseTextField';
