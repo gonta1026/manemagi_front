132ff4f307a7a0311981aced531f16804b45cf67
gonta1026 <katano.original@gmail.com>
2021-07-10 23:30:30 +0900

    update: 表示確認用のページに表示

--- src/pages/sample/index.tsx
+++ src/pages/sample/index.tsx
@@ -3,9 +3,11 @@ import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
 import { TPostState } from '../../types/Post';
 import CommonWrapTemplate from '../../components/common/template/CommonWrapTemplate';
+import { BaseButton } from '../../components/common/uiParts/atoms';
 import { fetchPosts } from '../../reducks/services/Post';
 
-const Home: React.FC = () => {
+// TODO いずれこちらのページは削除。表示角煮尿として使用
+const Sample: React.FC = () => {
   const dispatch = useDispatch();
   const { post, posts } = useSelector((state: { postState: TPostState }) => state.postState);
 
@@ -17,12 +19,48 @@ const Home: React.FC = () => {
 
   return (
     <CommonWrapTemplate>
-      <div>微修正</div>
+      <hr className="my-10" />
+      <h3>ボタンサンプル</h3>
+      <BaseButton
+        color="primary"
+        onClick={() => console.log('click')}
+        startIcon="addCircleOutline"
+        variant="contained"
+      >
+        登録
+      </BaseButton>
+      <BaseButton
+        color="primary"
+        onClick={() => console.log('click')}
+        startIcon="arrowBack"
+        variant="contained"
+      >
+        戻る
+      </BaseButton>
+      <BaseButton
+        color="inherit"
+        onClick={() => console.log('click')}
+        startIcon="update"
+        variant="contained"
+      >
+        更新
+      </BaseButton>
+      <BaseButton
+        color="secondary"
+        onClick={() => console.log('click')}
+        startIcon="delete"
+        variant="contained"
+      >
+        削除
+      </BaseButton>
+      <hr className="my-10" />
+      <h3>Reduxのサンプル実装</h3>
       {posts?.map((post, index) => (
         <p key={index}>{post.title}</p>
       ))}
+      <hr className="my-10" />
     </CommonWrapTemplate>
   );
 };
 
-export default Home;
+export default Sample;
