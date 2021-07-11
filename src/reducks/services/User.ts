933eafdcdb68522c56439123d635ad58efc07679
gonta1026 <katano.original@gmail.com>
2021-07-12 08:52:49 +0900

    fix: ログイン処理を微修正

--- src/reducks/services/User.ts
+++ src/reducks/services/User.ts
@@ -1,6 +1,6 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import ApiClient from '../../network/ApiClient';
-import { TUser } from '../../types/User';
+import { TUser, TLoginUser } from '../../types/User';
 import { END_POINT } from '../../const/endPoint';
 
 const { DEVISE_TOKEN_AUTH } = END_POINT;
@@ -25,19 +25,31 @@ export const signupUser = createAsyncThunk(
   },
 );
 
-export const loginUser = createAsyncThunk('user/loginUser', async (loginForm: TUser, thunkAPI) => {
-  try {
-    const response: any = await ApiClient.postRequest(
-      DEVISE_TOKEN_AUTH.REGISTRATIONS.CREATE,
-      loginForm,
-    );
-    const { headers } = response;
-    localStorage.setItem('accessToken', headers['access-token']);
-    localStorage.setItem('client', headers['client']);
-    localStorage.setItem('uid', headers['uid']);
-    return response.data;
-  } catch (error) {
-    console.log(error);
-    return thunkAPI.rejectWithValue({ errorMessage: error.message });
-  }
-});
+export const loginUser = createAsyncThunk(
+  'user/loginUser',
+  async (loginForm: TLoginUser, thunkAPI) => {
+    try {
+      const response: any = await ApiClient.postRequest(
+        DEVISE_TOKEN_AUTH.REGISTRATIONS.NEW,
+        loginForm,
+      );
+      console.log({ response });
+      const { headers, status, message } = response;
+      if (status === 401) {
+        return {
+          data: {
+            message: message,
+            status: status,
+          },
+        };
+      } else {
+        localStorage.setItem('accessToken', headers['access-token']);
+        localStorage.setItem('client', headers['client']);
+        localStorage.setItem('uid', headers['uid']);
+        return response.data;
+      }
+    } catch (error) {
+      return thunkAPI.rejectWithValue({ errorMessage: error.message });
+    }
+  },
+);
