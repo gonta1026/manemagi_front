a026dbb5cb2c3b98acc6bca9a2c54ae45ef1614d
gonta1026 <katano.original@gmail.com>
2021-07-11 22:41:51 +0900

    fix: 認証の処理を仮実装

--- src/components/common/template/CommonWrapTemplate.tsx
+++ src/components/common/template/CommonWrapTemplate.tsx
@@ -1,14 +1,13 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useEffect, useState } from 'react';
 import { BaseContainer } from '../uiParts/layout';
-import useIsAfterSsr from '../../../customHook/useIsAfterSsr';
 import { BaseHeader, Drawer } from '../organisms';
 import { useRouter } from 'next/router';
 
 const CommonWrapTemplate = ({ children }: { children: ReactNode }) => {
   const router = useRouter();
-  const isAfterSsr = useIsAfterSsr();
 
-  const [isDrawerOpen, setIsDrawerOpen] = React.useState(false);
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const [isDisplay, setIsDisplay] = useState(false);
 
   const toggleDrawer = (open: boolean) => (event: any) => {
     if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
@@ -17,30 +16,30 @@ const CommonWrapTemplate = ({ children }: { children: ReactNode }) => {
     setIsDrawerOpen(open);
   };
 
-  const pages = ['/signup', '/login', '/'];
-  const isDisplayAtLogined = () => {
-    if (isAfterSsr) {
-      console.log('router', router.pathname);
-      const noLoginedArrowPages = pages.filter((pageName) => pageName === router.pathname);
-      if (
-        // TODO ローカルストレージの処理は後に別箇所に移動させる。
-        // TODO // でたらめなtoken等をセットされた時にデータは取れないが画面にはいけてしまうのは問題な気もするのでできれば対応をしたい。。
-        localStorage.getItem('uid') &&
-        localStorage.getItem('accessToken') &&
-        localStorage.getItem('client') &&
-        noLoginedArrowPages.length > 0
-      ) {
-        return true;
-      } else {
-        router.push('/');
-        return false;
-      }
+  // Auth系のclassのところに処理を持ってくる。
+  const isLogindCheck = () => {
+    const pages = ['/signup', '/login', '/'];
+    const isCurrentPageCheck = pages.some((pageName) => pageName === router.pathname);
+    if (
+      isCurrentPageCheck &&
+      // localStorageの扱いもAuth系のクラスでメソッド化させたい。
+      localStorage.getItem('uid') &&
+      localStorage.getItem('accessToken') &&
+      localStorage.getItem('client')
+    ) {
+      setIsDisplay(true);
+    } else {
+      router.push('/login');
     }
   };
 
+  useEffect(() => {
+    isLogindCheck();
+  }, []);
+
   return (
     <>
-      {isDisplayAtLogined() && (
+      {isDisplay && (
         <>
           <BaseHeader toggleDrawer={toggleDrawer} />
           <Drawer isDrawerOpen={isDrawerOpen} toggleDrawer={toggleDrawer} />
