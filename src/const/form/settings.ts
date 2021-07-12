21b38c2e5f63c80654c4631a6f1bb6fcc445efc3
Syunki0403 <blackapple0403@gmail.com>
2021-07-12 23:00:19 +0900

    feat: エンドポイント追加、フォームファイル追加、型定義ファイル追加

---
+++ src/const/form/settings.ts
@@ -0,0 +1,10 @@
+export const LABEL_SETTINGS = {
+  LINE_NOTICE_TOKEN: 'LINEトークン',
+} as const;
+
+export const SETTINGSFORM = {
+  LINE_NOTICE_TOKEN: {
+    LABEL: LABEL_SETTINGS.LINE_NOTICE_TOKEN,
+    ID: 'lineNoticeToken',
+  },
+} as const;
