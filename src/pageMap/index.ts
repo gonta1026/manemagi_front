312fdce0f39cca1b904c197d83023c3e0292d464
gonta1026 <katano.original@gmail.com>
2021-07-20 07:54:55 +0900

    update: pageMapの調整

--- src/pageMap/index.ts
+++ src/pageMap/index.ts
@@ -1,37 +1,88 @@
-export const pageMap = [
+export const page = {
+  signup: {
+    name: () => '新規登録',
+    link: () => '/signup',
+  },
+  login: {
+    name: () => 'ログイン',
+    link: () => '/login',
+  },
+  top: {
+    name: () => 'トップ',
+    link: () => '/top',
+  },
+  shopping: {
+    list: {
+      name: () => '買い物一覧',
+      link: () => '/shopping',
+    },
+    register: {
+      name: () => '買い物登録',
+      link: () => '/shopping/new',
+    },
+  },
+  shop: {
+    list: {
+      name: () => 'お店一覧',
+      link: () => '/shop',
+    },
+    register: {
+      name: () => 'お店登録',
+      link: () => '/shop/new',
+    },
+  },
+  claim: {
+    list: {
+      name: () => '請求一覧',
+      link: () => '/claim',
+    },
+    register: {
+      name: () => '請求登録',
+      link: () => '/claim/new',
+    },
+  },
+  setting: {
+    edit: {
+      name: () => 'LINE設定',
+      link: () => '/setting',
+    },
+  },
+};
+
+export const drawerLinks = [
   {
-    name: '買い物一覧',
-    link: '/shopping',
+    name: page.shopping.list.name(),
+    link: page.shopping.list.link(),
     icon: 'shoppingCart',
   },
   {
-    name: '買い物登録',
-    link: '/shopping/new',
+    name: page.shopping.register.name(),
+    link: page.shopping.register.link(),
     icon: '',
   },
   {
-    name: 'お店一覧',
-    link: '/shop',
+    name: page.shop.list.name(),
+    link: page.shop.list.link(),
     icon: 'store',
   },
   {
-    name: 'お店登録',
-    link: '/shop/new',
+    name: page.shop.register.name(),
+    link: page.shop.register.link(),
     icon: '',
   },
   {
-    name: '請求一覧',
-    link: '/claim',
+    name: page.claim.list.name(),
+    link: page.claim.list.link(),
     icon: 'money',
   },
   {
-    name: '請求登録',
-    link: '/claim/new',
+    name: page.claim.register.name(),
+    link: page.claim.register.link(),
     icon: '',
   },
   {
-    name: '設定',
-    link: '/setting',
+    name: page.setting.edit.name(),
+    link: page.setting.edit.link(),
     icon: 'settings',
   },
 ] as const;
