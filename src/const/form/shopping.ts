813c1430725a925f8bc0d67efeeb4662627d366d
gonta1026 <katano.original@gmail.com>
2021-07-22 00:24:36 +0900

    feat: shoppingのformに使う定数を作成

---
+++ src/const/form/shopping.ts
@@ -0,0 +1,30 @@
+export const LABEL_SHOPPING = {
+  PRICE: '金額',
+  DATE: '買い物日',
+  DESCRIPTION: '説明',
+  IS_LINE_NOTICE: 'LINEの通知',
+  SHOP_ID: 'お店',
+} as const;
+
+export const SHOPPINGFORM = {
+  PRICE: {
+    LABEL: LABEL_SHOPPING.PRICE,
+    ID: 'price',
+  },
+  DATE: {
+    LABEL: LABEL_SHOPPING.DATE,
+    ID: 'date',
+  },
+  DESCRIPTION: {
+    LABEL: LABEL_SHOPPING.DESCRIPTION,
+    ID: 'description',
+  },
+  IS_LINE_NOTICE: {
+    LABEL: LABEL_SHOPPING.IS_LINE_NOTICE,
+    ID: 'isLineNotice',
+  },
+  SHOP_ID: {
+    LABEL: LABEL_SHOPPING.SHOP_ID,
+    ID: 'shopId',
+  },
+} as const;
