6d82d11a43b34336f8a7c72e5ff46fd7a280c993
gonta1026 <katano.original@gmail.com>
2021-07-23 14:10:57 +0900

    feat: eslintでno-non-null-assertionを許可させた。

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,5 +20,7 @@ module.exports = {
   rules: {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    // 論理否定演算子を使用したnull以外のアサーションを許可する
+    '@typescript-eslint/no-non-null-assertion': 'off',
   },
 };
