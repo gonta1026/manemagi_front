fb64b7c94700dab3614e9e5c76861b8cbb2da29b
gonta1026 <katano.original@gmail.com>
2021-07-24 07:29:44 +0900

    feat: 3点リード付与の関数を作成

--- src/utils/function.ts
+++ src/utils/function.ts
@@ -1,1 +1,8 @@
 export const isEmpty = (obj: any) => !Object.keys(obj).length;
+
+export const ommisionText = (text: string, length = 10): string => {
+  if (text.length <= length) {
+    return text;
+  }
+  return text.substring(0, length) + '...';
+};
