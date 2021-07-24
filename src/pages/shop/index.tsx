7f6000a3c8aad3af9614386ea0a0c1c1a1d8da75
gonta1026 <katano.original@gmail.com>
2021-07-24 21:24:54 +0900

    fix: お店ページ修正

--- src/pages/shop/index.tsx
+++ src/pages/shop/index.tsx
@@ -2,14 +2,13 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 /* components */
 import CommonWrapTemplate from '../../components/common/template/CommonWrapTemplate';
-import { BasePageTitle, BaseLink } from '../../components/common/uiParts/atoms';
+import { BasePageTitle } from '../../components/common/uiParts/atoms';
 /* pageMap */
 import { page } from '../../pageMap';
 /* reducks */
 import { fetchShops } from '../../reducks/services/Shop';
 /* types */
 import { TShop } from '../../types/Shop';
-import { ommisionText } from '../../utils/function';
 
 const Shop = (): JSX.Element => {
   const [shops, setShopNames] = useState<TShop[]>([]);
@@ -28,15 +27,15 @@ const Shop = (): JSX.Element => {
   return (
     <CommonWrapTemplate>
       <BasePageTitle className={'my-5'}>{page.shop.list.name()}</BasePageTitle>
-      <p>一旦一覧画面を作成、これからどのようにカスタマイズするか等を検討</p>
+      <p>
+        一旦一覧画面を作成、これからどのようにカスタマイズするか等を検討。お店の情報も少ないので詳細画面は作らない予定。
+      </p>
       <ul className="py-4">
         {shops.map((shop, index) => (
-          <BaseLink key={index} pathname={page.shop.detail.link(shop.id)}>
-            <li className={'border-t-2 p-3'}>
-              <div>店名：{shop.name}</div>
-              <div>説明：{ommisionText(shop.description, 15)}</div>
-            </li>
-          </BaseLink>
+          <li key={index} className={'border-t-2 p-3'}>
+            <div>店名：{shop.name}</div>
+            <div>説明：{shop.description}</div>
+          </li>
         ))}
       </ul>
     </CommonWrapTemplate>
