f4f56d9e556b2b361c2cc54b5a93ada833a5db80
gonta1026 <katano.original@gmail.com>
2021-07-25 07:21:02 +0900

    fix: ボタン群コンポーネントの微修正

--- src/components/common/molecules/button/ExecutionAndBackButtons.tsx
+++ src/components/common/molecules/button/ExecutionAndBackButtons.tsx
@@ -6,6 +6,7 @@ type TProps = {
   backPathname: string;
   backButtonName: string;
   disabledExecution?: boolean;
+  nextPathname?: string;
   nextButtonName: string;
 };
 
@@ -14,25 +15,34 @@ const ExecutionAndBackButtons = ({
   backPathname,
   backButtonName,
   disabledExecution,
+  nextPathname,
   nextButtonName,
 }: TProps) => {
   return (
     <>
-      <div className={className}>
-        <BaseButton
-          color={'primary'}
-          disabled={disabledExecution}
-          type={'submit'}
-          variant={'contained'}
-        >
-          {nextButtonName}
-        </BaseButton>
+      <div className={className + ' flex justify-center'}>
+        {nextPathname ? (
+          <BaseLink pathname={nextPathname}>
+            <BaseButton color={'primary'} variant={'contained'}>
+              {nextButtonName}
+            </BaseButton>
+          </BaseLink>
+        ) : (
+          <BaseButton
+            color={'primary'}
+            disabled={disabledExecution}
+            type={'submit'}
+            variant={'contained'}
+          >
+            {nextButtonName}
+          </BaseButton>
+        )}
       </div>
       <hr className="my-5" />
       <div className="flex justify-center">
         <BaseLink pathname={backPathname}>
           <BaseButton color={'secondary'} variant={'contained'}>
-            {backButtonName}へ戻る
+            {backButtonName}
           </BaseButton>
         </BaseLink>
       </div>
