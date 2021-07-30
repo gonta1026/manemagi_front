41c02b2a9daaecee324641d4b1e30120eff9ab37
gonta1026 <katano.original@gmail.com>
2021-07-30 21:43:12 +0900

    feat: backdrop 修正

--- src/components/common/molecules/index.ts
+++ src/components/common/molecules/index.ts
@@ -3,5 +3,5 @@ export { default as LabelAndTextField } from './form/LabelAndTextField';
 export { default as LabelAndTextArea } from './form/LabelAndTextArea';
 export { default as LabelAndSelect } from './form/LabelAndSelect';
 export { default as LabelAndSwitch } from './form/LabelAndSwitch';
-export { default as BaseLoading } from './loading';
+export { default as BaseLoading } from './loading/BaseLoading';
 export { default as BaseToast } from './toast/BaseToast';
