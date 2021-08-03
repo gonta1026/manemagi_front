3c208005c83f5fb7ab7d7485889df02886688967
gonta1026 <katano.original@gmail.com>
2021-08-03 09:26:05 +0900

    feat: 請求中一覧画面の仮作成

---
+++ src/pages/claim/[Id]/shopping/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import CommonWrapTemplate from '../../../../components/common/template/CommonWrapTemplate';
+
+const ClaimShoppings = (): JSX.Element => {
+  return <CommonWrapTemplate>ClaimShoppings 請求中の買い物一覧を出力</CommonWrapTemplate>;
+};
+
+export default ClaimShoppings;
