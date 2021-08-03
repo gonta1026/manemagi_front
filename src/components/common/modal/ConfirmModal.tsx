6f52d8c1bcf453e766d171adc869f0b7f6bd3aca
gonta1026 <katano.original@gmail.com>
2021-08-03 23:03:05 +0900

    update: Modalコンポーネントを汎用的に修正

--- src/components/common/modal/ConfirmModal.tsx
+++ src/components/common/modal/ConfirmModal.tsx
@@ -2,24 +2,51 @@ import React, { ReactNode } from 'react';
 import styled from 'styled-components';
 import BaseModal from './BaseModal';
 import { CloseButton, BaseButton } from '../uiParts/atoms';
+import { TIconType } from '../uiParts/atoms/button/BaseSwitchIcon';
 
+type TModalTitle = '入力' | '変更' | '削除' | '請求受領';
 interface TProps {
   open: boolean;
   handleClose: VoidFunction;
   handleOk: VoidFunction;
   children: ReactNode;
+  modaltitle?: TModalTitle;
 }
 
-const ConfirmModal = ({ children, handleClose, handleOk, open }: TProps): JSX.Element => {
+const ConfirmModal = ({
+  children,
+  handleClose,
+  handleOk,
+  open,
+  modaltitle = '入力',
+}: TProps): JSX.Element => {
+  let buttonName = '';
+  let customType: TIconType = 'normal';
+  switch (modaltitle) {
+    case '入力':
+    case '請求受領':
+      buttonName = '登録';
+      customType = 'addCircleOutline';
+      break;
+    case '変更':
+      buttonName = '更新';
+      customType = 'update';
+      break;
+    case '削除':
+      buttonName = '削除';
+      customType = 'delete';
+      break;
+  }
+
   return (
     <BaseModal {...{ handleClose, open }}>
+      <CloseButton className={'right-2 top-3 absolute'} handleClose={handleClose} />
       <Wrap className="Wrap">
-        <CloseButton className={'right-1 top-1'} handleClose={handleClose} />
-        <h4 className={'font-bold text-center text-lg my-3'}>入力確認</h4>
+        <h4 className={'font-bold text-center text-lg my-3'}>{modaltitle}確認</h4>
         {children}
         <div className="flex justify-center mb-3 mt-5">
-          <BaseButton focus onClick={handleOk} type={'submit'} customType={'addCircleOutline'}>
-            登録
+          <BaseButton focus onClick={handleOk} type={'submit'} customType={customType}>
+            {buttonName}
           </BaseButton>
           <BaseButton className={'ml-5'} onClick={handleClose} customType={'arrowBack'}>
             戻る
