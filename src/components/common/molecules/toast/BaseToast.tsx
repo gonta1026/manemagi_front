56f295d6625b179adfe76cde82c94d98f745b3d0
gonta1026 <katano.original@gmail.com>
2021-08-09 19:42:35 +0900

    fix: toastのスタイルを調整

--- src/components/common/molecules/toast/BaseToast.tsx
+++ src/components/common/molecules/toast/BaseToast.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Snackbar from '@material-ui/core/Snackbar';
 import MuiAlert from '@material-ui/lab/Alert';
 import { OmitTToastType } from '../../../../customHook/useToastAction';
+import { materialStyles } from '../../../../styles/js/material';
 
 function Alert(props: any) {
   return <MuiAlert elevation={6} variant="filled" {...props} />;
@@ -11,13 +12,30 @@ const BaseToast = ({
   open,
   message,
   severity = 'info',
-  autoHideDuration = 4000,
+  autoHideDuration,
   onClose,
 }: OmitTToastType) => {
+  // NOTE  スマホ表示の際に崩れる現象があったので非表示で対応
+  const classes = materialStyles({
+    alert: {
+      '& .MuiAlert-icon': {
+        '@media (max-width: 768px)': {
+          display: 'none',
+        },
+      },
+      '& .MuiAlert-action': {
+        '@media (max-width: 768px)': {
+          display: 'none',
+        },
+      },
+    },
+  });
   const anchorOrigin = { horizontal: 'right', vertical: 'top' } as const;
   return (
     <Snackbar {...{ anchorOrigin, autoHideDuration, onClose, open }}>
-      <Alert {...{ onClose, severity }}>{message}</Alert>
+      <Alert className={classes.alert} {...{ onClose, severity }}>
+        {message}
+      </Alert>
     </Snackbar>
   );
 };
