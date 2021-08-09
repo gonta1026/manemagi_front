f3add3e1fb411741ffacdd74ba1840fe4a9bc40a
gonta1026 <katano.original@gmail.com>
2021-08-09 11:02:52 +0900

    fix: lineのvalidationのmaxNum誤りを修正

--- src/validate/setting/setting.ts
+++ src/validate/setting/setting.ts
@@ -4,12 +4,12 @@ import { validMaxNum, validNum } from '../';
 import { lineToken } from '../message';
 export const settingsValidate = (values: TSetting, errors: TSettingFormError) => {
   const { LINE_NOTICE_TOKEN, IS_USE_LINE } = SETTING_FORM;
-  const { MAX_50 } = validNum;
+  const { MAX_100 } = validNum;
   /******************
    * LINEトークン
    ******************/
-  if (validMaxNum.check(values.lineNoticeToken, MAX_50)) {
-    errors.lineNoticeToken = validMaxNum.message(LINE_NOTICE_TOKEN.LABEL, MAX_50);
+  if (validMaxNum.check(values.lineNoticeToken, MAX_100)) {
+    errors.lineNoticeToken = validMaxNum.message(LINE_NOTICE_TOKEN.LABEL, MAX_100);
   }
   // NOTE 複合的な処理なので現状は当ファイルに記述
   if (values.isUseLine && values.lineNoticeToken === '') {
