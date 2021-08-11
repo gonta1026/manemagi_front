57a2dcca5a3fe31cd1b996627c9d332a7b1d436f
gonta1026 <katano.original@gmail.com>
2021-08-12 08:52:06 +0900

    update: endpointのリファクタリング

--- src/const/endPoint.ts
+++ src/const/endPoint.ts
@@ -1,31 +1,37 @@
 // railsのコントローラーに一致する形でエンドポイントを整理している。こちらは試作で作っているので使わない可能性もあり。（2021/6/26）
+const auth = 'auth';
+const shops = 'shops';
+const shoppings = 'shoppings';
+const claims = 'claims';
+const settings = 'settings';
+
 export const END_POINT = {
   DEVISE_TOKEN_AUTH: {
     REGISTRATIONS: {
-      CREATE: '/auth', // ユーザー新規登録
-      NEW: '/auth/sign_in', // ユーザーログイン
+      CREATE: `/${auth}`, // ユーザー新規登録
+      NEW: `/${auth}/sign_in`, // ユーザーログイン
     },
   },
   SHOPS: {
-    INDEX: '/shops', // ショップ登録
-    CREATE: '/shops', // ショップ登録
+    INDEX: `/${shops}`, // ショップ登録
+    CREATE: `/${shops}`, // ショップ登録
   },
   SHOPPINGS: {
-    INDEX: '/shoppings', // ショップ一覧
-    CREATE: '/shoppings', // 買い物登録
-    SHOW: (id: number | string) => `/shoppings/${id}`, // 買い物詳細
-    EDIT: (id: number | string) => `/shoppings/${id}/edit`, // 買い物編集
-    UPDATE: (id: number | string) => `/shoppings/${id}`, // 買い物編集
-    DELETE: (id: number | string) => `/shoppings/${id}`, // 買い物削除
+    INDEX: `/${shoppings}`, // ショップ一覧
+    CREATE: `/${shoppings}`, // 買い物登録
+    SHOW: (id: number | string) => `/${shoppings}/${id}`, // 買い物詳細
+    EDIT: (id: number | string) => `/${shoppings}/${id}/edit`, // 買い物編集
+    UPDATE: (id: number | string) => `/${shoppings}/${id}`, // 買い物編集
+    DELETE: (id: number | string) => `/${shoppings}/${id}`, // 買い物削除
   },
   CLAIMS: {
-    INDEX: '/claims', // 請求一覧
-    CREATE: '/claims', // 請求登録
-    NEW: '/claims/new', // 請求登録
-    UPDATE: (id: number | string) => `/claims/${id}`, // 請求受領
+    INDEX: `/${claims}`, // 請求一覧
+    CREATE: `/${claims}`, // 請求登録
+    NEW: `/${claims}/new`, // 請求登録
+    UPDATE: (id: number | string) => `/${claims}/${id}`, // 請求受領
   },
   SETTINGS: {
-    INDEX: '/settings',
-    UPDATE: '/settings',
+    INDEX: `/${settings}`,
+    UPDATE: `/${settings}`,
   },
 } as const;
