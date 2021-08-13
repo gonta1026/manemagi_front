b5b83757c3ebf17293eef9ed06e15f6bf2159214
gonta1026 <katano.original@gmail.com>
2021-08-13 20:22:36 +0900

    feat: 削除用のエンドポイントを作成、型調整

--- src/const/endPoint.ts
+++ src/const/endPoint.ts
@@ -18,17 +18,18 @@ export const END_POINT = {
   SHOPPINGS: {
     INDEX: `/${shoppings}`, // ショップ一覧
     CREATE: `/${shoppings}`, // 買い物登録
-    SHOW: (id: number | string) => `/${shoppings}/${id}`, // 買い物詳細
-    EDIT: (id: number | string) => `/${shoppings}/${id}/edit`, // 買い物編集
-    UPDATE: (id: number | string) => `/${shoppings}/${id}`, // 買い物編集
-    DELETE: (id: number | string) => `/${shoppings}/${id}`, // 買い物削除
+    SHOW: (id: string) => `/${shoppings}/${id}`, // 買い物詳細
+    EDIT: (id: string) => `/${shoppings}/${id}/edit`, // 買い物編集
+    UPDATE: (id: string) => `/${shoppings}/${id}`, // 買い物編集
+    DESTROY: (id: string) => `/${shoppings}/${id}`, // 買い物削除
   },
   CLAIMS: {
     INDEX: `/${claims}`, // 請求一覧
     CREATE: `/${claims}`, // 請求登録
     NEW: `/${claims}/new`, // 請求登録
-    UPDATE: (id: number | string) => `/${claims}/${id}`, // 請求受領
-    SHOPPINGS: (claimId: number | string) => `/${claims}/${claimId}/${shoppings}`, // 請求内訳一覧
+    UPDATE: (id: string) => `/${claims}/${id}`, // 請求受領
+    DESTROY: (id: string) => `/${claims}/${id}`, // 請求解除
+    SHOPPINGS: (claimId: string) => `/${claims}/${claimId}/${shoppings}`, // 請求内訳一覧
   },
   SETTINGS: {
     INDEX: `/${settings}`,
