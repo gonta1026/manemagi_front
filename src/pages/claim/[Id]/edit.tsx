254326ba5204216a47d2de9c96aae6aeab2a4148
gonta1026 <katano.original@gmail.com>
2021-08-14 13:50:23 +0900

    feat: 請求買い物編集画面削除

--- src/pages/claim/[Id]/edit.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import CommonWrapTemplate from '../../../components/common/layout/CommonWrapTemplate';
-
-const ShoppingEdit = (): JSX.Element => {
-  return <CommonWrapTemplate>Shopping編集ページ</CommonWrapTemplate>;
-};
-
-export default ShoppingEdit;
