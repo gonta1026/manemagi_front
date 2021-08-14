9576ac8f92a00a85e213f778c200cb650500426e
gonta1026 <katano.original@gmail.com>
2021-08-14 11:00:48 +0900

    feat: topページをカスタムフックにて共通化

--- src/customHook/claim/useClaim.tsx
+++ src/customHook/claim/useClaim.tsx
@@ -1,12 +1,12 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { TClaim } from '../../types/Claim';
-import { fetchClaims } from '../../reducks/services/Claim';
+import { fetchClaims, updateClaim, deleteClaim } from '../../reducks/services/Claim';
+import { ToastType } from '../../customHook/useToastAction';
 
 const useClaim = () => {
   const [claims, setClaims] = useState<TClaim[]>([]);
   const dispatch = useDispatch();
-
   const fetchClaimsAndSet = async () => {
     const response: any = await dispatch(fetchClaims());
     if (response.payload.status === 'success') {
@@ -15,7 +15,63 @@ const useClaim = () => {
     }
   };
 
-  return { claims, fetchClaimsAndSet };
+  const updateClaimAndSet = async (
+    claim: TClaim,
+    isLineNotice: boolean,
+    toastActions: ToastType,
+  ) => {
+    const claimId = String(claim.id);
+    const response: any = await dispatch(
+      updateClaim({
+        id: claimId,
+        data: { isLineNotice, isReceipt: true },
+      }),
+    );
+
+    const { handleToastOpen } = toastActions;
+    console.log(response);
+    if (response.payload.status === 'success') {
+      fetchClaimsAndSet();
+      handleToastOpen({
+        message: `請求受領を登録しました。`,
+        severity: 'success',
+      });
+    } else {
+      handleToastOpen({
+        message: `請求受領の登録に失敗しました。`,
+        severity: 'error',
+      });
+    }
+  };
+
+  const deleteClaimAndSet = async (
+    claim: TClaim,
+    isLineNotice: boolean,
+    toastActions: ToastType,
+  ) => {
+    const claimId = String(claim.id);
+    const response: any = await dispatch(
+      deleteClaim({
+        id: claimId,
+        data: { isLineNotice: isLineNotice },
+      }),
+    );
+    const { handleToastOpen } = toastActions;
+    if (response.payload.status === 'success') {
+      fetchClaimsAndSet();
+      handleToastOpen({
+        message: `請求を削除しました。`,
+        severity: 'success',
+      });
+    } else {
+      handleToastOpen({
+        message: `削除に失敗しました。`,
+        severity: 'error',
+      });
+    }
+  };
+
+  return { claims, fetchClaimsAndSet, updateClaimAndSet, deleteClaimAndSet };
 };
 
 export default useClaim;
--- src/customHook/shopping/useShopping.tsx
+++ src/customHook/shopping/useShopping.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { TShopping } from '../../types/Shopping';
-import { fetchShoppings } from '../../reducks/services/Shopping';
+import { fetchShoppings, deleteShopping } from '../../reducks/services/Shopping';
 
 const useShopping = () => {
   const [shoppings, setShopping] = useState<TShopping[]>([]);
@@ -16,7 +16,44 @@ const useShopping = () => {
     }
   };
 
-  return { shoppings, fetchNoClaimShoppingsAndSet };
+  const fetchShoppingsAndSet = async () => {
+    const response: any = await dispatch(fetchShoppings());
+    if (response.payload.status === 'success') {
+      const shoppings: TShopping[] = response.payload.data;
+      const noClaimShoppings = shoppings.filter((shopping) => shopping.claimId === null);
+      setShopping(noClaimShoppings);
+    }
+  };
+
+  const deleteShoppingAndSet = async (
+    shopping: TShopping,
+    isLineNotice: boolean,
+    toastActions: any,
+  ) => {
+    const shoppingId = String(shopping.id);
+    const response: any = await dispatch(
+      deleteShopping({
+        id: shoppingId,
+        data: { isLineNotice },
+      }),
+    );
+
+    const { handleToastOpen } = toastActions;
+    if (response.payload.status === 'success') {
+      fetchNoClaimShoppingsAndSet();
+      handleToastOpen({
+        message: `買い物を削除しました。`,
+        severity: 'success',
+      });
+    } else {
+      handleToastOpen({
+        message: `削除に失敗しました。`,
+        severity: 'error',
+      });
+    }
+  };
+
+  return { shoppings, fetchShoppingsAndSet, fetchNoClaimShoppingsAndSet, deleteShoppingAndSet };
 };
 
 export default useShopping;
