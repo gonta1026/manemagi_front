742008d9fc00a0d9a291447a727d2fceac8c98d9
gonta1026 <katano.original@gmail.com>
2021-08-22 08:01:15 +0900

    fix: apiclientでのtry-catchを削除

--- src/modules/ApiClient.ts
+++ src/modules/ApiClient.ts
@@ -3,14 +3,14 @@ import camelcaseKeys from 'camelcase-keys';
 import snakecaseKeys from 'snakecase-keys';
 import Auth from './Auth';
 
-type NormalizeError = {
-  data: {
-    status: number;
-    message: string;
-    statusText: string;
-    raw: any;
-  };
-};
+// type NormalizeError = {
+//   data: {
+//     status: number;
+//     message: string;
+//     statusText: string;
+//     raw: any;
+//   };
+// };
 
 class APIClient {
   private readonly axiosInstance: AxiosInstance;
@@ -38,14 +38,9 @@ class APIClient {
   }
 
   public async getRequest(this: APIClient, url: string, config?: AxiosRequestConfig) {
-    try {
-      const response = await this.axiosInstance.get(url, config);
-      const normalizeResponse = this.toCamelcaseKeys(response);
-      return normalizeResponse;
-    } catch (e) {
-      const error = this._normalizeError(e);
-      return error;
-    }
+    const response = await this.axiosInstance.get(url, config);
+    const normalizeResponse = this.toCamelcaseKeys(response);
+    return normalizeResponse;
   }
 
   public async patchRequest(
@@ -54,58 +49,35 @@ class APIClient {
     params: any,
     config?: AxiosRequestConfig,
   ) {
-    try {
-      const snakeParams: any = snakecaseKeys(params);
-      const response = await this.axiosInstance.patch(url, snakeParams, config);
-      const normalizeResponse = this.toCamelcaseKeys(response);
-      return normalizeResponse;
-    } catch (e) {
-      const error = this._normalizeError(e);
-      return error;
-    }
-  }
-
-  public async postRequest(this: APIClient, url: string, params: any, config?: AxiosRequestConfig) {
-    try {
-      const snakeParams: any = snakecaseKeys(params);
-      const response = await this.axiosInstance.post(url, snakeParams, config);
-      const normalizeResponse = this.toCamelcaseKeys(response);
-      return normalizeResponse;
-    } catch (e) {
-      const error = this._normalizeError(e);
-      return error;
-    }
-  }
-
-  public async deleteRequest(this: APIClient, url: string, config?: AxiosRequestConfig) {
-    try {
-      const data: any = snakecaseKeys(config?.data);
-      const response = await this.axiosInstance.delete(url, { ...config, data: data });
-      const normalizeResponse = this.toCamelcaseKeys(response);
-      return normalizeResponse;
-    } catch (e) {
-      const error = this._normalizeError(e);
-      return error;
-    }
-  }
-
-  private toCamelcaseKeys(response: AxiosResponse<any>) {
-    const data = camelcaseKeys({ ...response.data }, { deep: true }); // NOTE deepによりネストされた箇所も変換対象となる。
-    const normalizeResponse = { ...response, data };
+    const snakeParams: any = snakecaseKeys(params);
+    const response = await this.axiosInstance.patch(url, snakeParams, config);
+    const normalizeResponse = this.toCamelcaseKeys(response);
     return normalizeResponse;
   }
 
-  private _normalizeError(error: any) {
-    const normalizeError: NormalizeError = {
-      // axiosの『data』でのアクセスに合わせてdataで囲む
-      data: {
-        status: error.response && error.response.status,
-        statusText: error.response.statusText,
-        message: error.message,
-        raw: error,
-      },
-    };
-    return normalizeError;
+  public async postRequest<T = any>(
+    this: APIClient,
+    url: string,
+    params: any,
+    config?: AxiosRequestConfig,
+  ) {
+    const snakeParams = snakecaseKeys(params);
+    const response = await this.axiosInstance.post<T>(url, snakeParams, config);
+    const normalizeResponse = this.toCamelcaseKeys(response);
+    return normalizeResponse;
+  }
+
+  public async deleteRequest(this: APIClient, url: string, config?: AxiosRequestConfig) {
+    const data: any = snakecaseKeys(config?.data);
+    const response = await this.axiosInstance.delete(url, { ...config, data: data });
+    const normalizeResponse = this.toCamelcaseKeys(response);
+    return normalizeResponse;
+  }
+
+  private toCamelcaseKeys<T>(response: AxiosResponse<T>) {
+    const data = camelcaseKeys({ ...response.data }, { deep: true }); // NOTE deepによりネストされた箇所も変換対象となる。
+    const normalizeResponse = { ...response, data };
+    return normalizeResponse;
   }
 }
 
