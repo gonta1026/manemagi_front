fd342e42061499c367d4551290635f94d704fda7
gonta1026 <katano.original@gmail.com>
2021-08-24 06:59:41 +0900

    feat: utiliryのmaped typeを作成

--- src/@types/utility/index.d.ts
+++ src/@types/utility/index.d.ts
@@ -1,5 +1,13 @@
 declare namespace Utilty {
+  // 指定をしたKeysで指定をしたユニオンをnullを合わせたユニオン型へする変更をさせる。
   export type Nullable<T, Keys> = {
-    [P in keyof T]: Keys extends P ? T[P] | null : T[P];
+    [P in keyof T]: P extends Keys ? T[P] | null : T[P];
+  };
+  export type SelectChange<T, Keys, U> = {
+    [P in keyof T]: P extends Keys ? U : T[P];
+  };
+  // 指定をしたkeyを新しい方に変更をさせる。
+  export type Change<T, Key, NewType> = {
+    [P in keyof T]: Key extends P ? NewType : T[P];
   };
 }
