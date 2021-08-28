42ede9b524ceee05024149e35a0dd5afacaf07db
gonta1026 <katano.original@gmail.com>
2021-08-28 14:27:23 +0900

    feat: payloadを対応

--- src/customHook/shop/useShop.tsx
+++ src/customHook/shop/useShop.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { PayloadAction } from '@reduxjs/toolkit';
 import { TShop, ResponseFetchShops } from '../../model/shop';
 import { fetchShops } from '../../reducks/services/Shop';
 
@@ -8,7 +9,7 @@ const useShop = () => {
   const dispatch = useDispatch();
 
   const fetchShopsAndSet = async () => {
-    const response: { payload: ResponseFetchShops } = await dispatch(fetchShops() as any);
+    const response: PayloadAction<ResponseFetchShops> = await dispatch(fetchShops() as any);
     if (response.payload.status === 'success') {
       const shops = response.payload.data;
       setShops(shops);
